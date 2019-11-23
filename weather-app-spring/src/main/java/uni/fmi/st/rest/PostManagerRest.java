6c777db2db1c7566a13ff84f1188e1902b0089e4
Ivan Zhelev <nvjcompany@gmail.com>
2019-11-23 15:43:24 +0200

    Create/show public posts

--- weather-app-spring/src/main/java/uni/fmi/st/rest/PostManagerRest.java
+++ weather-app-spring/src/main/java/uni/fmi/st/rest/PostManagerRest.java
@@ -29,35 +29,29 @@ public class PostManagerRest {
 		this.postRepo = postRepo;
 		this.userRepo = userRepo;
 	}
-	
+
 	@PostMapping("/removeMyPost")
-	public ResponseEntity<String> removePost
-						(@RequestParam(name = "id") int id,
-										HttpSession session){
+	public ResponseEntity<String> removePost(@RequestParam(name = "id") int id, HttpSession session) {
 		final User user = (User) session.getAttribute("currentUser");
 		if (null == user) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-					.body("");
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("");
 		}
 		List<Post> posts = user.getPosts();
-		Post postForRemove = posts.stream()
-							.filter(post->id == post.getId())
-							.findFirst().orElse(null);
-		if(null != postForRemove) {
+		Post postForRemove = posts.stream().filter(post -> id == post.getId()).findFirst().orElse(null);
+		if (null != postForRemove) {
 			posts.remove(postForRemove);
-			//session.setAttribute("currentUser", userRepo.save(user));
+			// session.setAttribute("currentUser", userRepo.save(user));
 			postRepo.delete(postForRemove);
 		}
-		return ResponseEntity.ok().body("Post with id: "+ id + " is removed");
+		return ResponseEntity.ok().body("Post with id: " + id + " is removed");
 	}
-	
+
 	@GetMapping("/getPosts")
 	public ResponseEntity<List<Post>> getAllPosts(HttpSession session) {
 		final List<Post> posts = new ArrayList<>();
 		final User user = (User) session.getAttribute("currentUser");
 		if (null == user) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-					.body(posts);
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(posts);
 		} else {
 			posts.addAll(postRepo.findByPublicPost(user));
 		}
@@ -65,14 +59,19 @@ public class PostManagerRest {
 	}
 
 	@PostMapping("/createPost")
-	public ResponseEntity<Post> createPost(@RequestParam(name = "comment") String comment,
-			@RequestParam(name = "place") String place, @RequestParam(name = "temp") float temp, HttpSession session) {
+	public ResponseEntity<Post> createPost(
+			@RequestParam(name = "comment") String comment,
+			@RequestParam(name = "place") String place, 
+			@RequestParam(name = "temp") float temp,
+			@RequestParam(name = "publicPost") Boolean isPublic,
+			HttpSession session) {
 
 		final User user = (User) session.getAttribute("currentUser");
 		if (null == user) {
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
 		}
 		final Post post = new Post(comment, place, temp);
+		post.setPublicPost(isPublic);
 		post.setOwner(user);
 		user.addPost(post);
 		session.setAttribute("currentUser", userRepo.save(user));
