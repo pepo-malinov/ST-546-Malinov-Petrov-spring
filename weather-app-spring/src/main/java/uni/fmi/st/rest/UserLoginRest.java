e14a700840b06a7a88683bb46709361a6f53e737
Ivan Zhelev <nvjcompany@gmail.com>
2019-11-24 16:09:01 +0200

    Generate user token on login

--- weather-app-spring/src/main/java/uni/fmi/st/rest/UserLoginRest.java
+++ weather-app-spring/src/main/java/uni/fmi/st/rest/UserLoginRest.java
@@ -3,9 +3,16 @@ package uni.fmi.st.rest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import uni.fmi.st.models.User;
 import uni.fmi.st.repos.UserRepository;
@@ -14,10 +21,13 @@ import uni.fmi.st.repos.UserRepository;
 public class UserLoginRest {
 
 	private UserRepository repository;
+	private UserDetailsService userDetailsService;
 
 	@Autowired
-	public UserLoginRest(final UserRepository repository) {
+	public UserLoginRest(final UserRepository repository,
+						UserDetailsService userDetailsService ) {
 		this.repository = repository;
+		this.userDetailsService = userDetailsService;
 	}
 
 	@PostMapping(value = "/login")
@@ -27,6 +37,22 @@ public class UserLoginRest {
 							.findByEmailAndPassword(email, password);
 		if (null != currentUser) {
 			session.setAttribute("currentUser", currentUser);
+			UserDetails securityUser = userDetailsService
+					.loadUserByUsername(currentUser.getUsername());
+			if(null == securityUser) {
+				securityUser = userDetailsService
+						.loadUserByUsername("user");
+			}
+			Authentication authentication = new 
+					UsernamePasswordAuthenticationToken(securityUser.getUsername(),
+							securityUser.getPassword());
+			SecurityContextHolder.getContext()
+							.setAuthentication(authentication);
+			ServletRequestAttributes attr = (ServletRequestAttributes)
+					RequestContextHolder.currentRequestAttributes();
+			HttpSession httpSession = attr.getRequest().getSession(true); // true == allow create
+			httpSession.setAttribute("SPRING_SECURITY_CONTEXT", SecurityContextHolder.getContext());
+
 		}
 		return "profile.html";
 	}
--- weather-app-spring/src/main/java/uni/fmi/st/security/SecurityConfig.java
+++ weather-app-spring/src/main/java/uni/fmi/st/security/SecurityConfig.java
@@ -5,11 +5,16 @@ package uni.fmi.st.security;
 
 import org.springframework.boot.actuate.autoconfigure.security
 											.servlet.EndpointRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * Base spring security config class
@@ -23,15 +28,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-		.authorizeRequests().antMatchers("/home.html").hasRole("ADMIN")
+		.authorizeRequests()
+		//.antMatchers("/home.html").hasRole("ADMIN")
 		.regexMatchers(HttpMethod.POST, "/removeMyPosts")
 		.hasAnyRole("ADMIN", "USER")
-		.regexMatchers(HttpMethod.POST, "/register")
-		.hasAnyRole("ADMIN", "USER")
+		/*.regexMatchers(HttpMethod.POST, "/register")
+		.hasAnyRole("ADMIN", "USER")*/
 		.antMatchers("/**").permitAll()
 		.requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
 		.and().csrf().disable();
 	}
 	
 	
+	@Bean
+	@Override
+	protected UserDetailsService userDetailsService() {
+		UserDetails user = User.withUsername("user")
+							.password("password")
+							.roles("USER")
+							.build();
+		UserDetails admin = User.withUsername("admin")
+							.password("password")
+							.roles("USER", "ADMIN")
+							.build();
+		return new InMemoryUserDetailsManager(user, admin);
+	}
+
 }
