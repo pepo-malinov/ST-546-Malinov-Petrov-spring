42c8defe01256a5e54ed62f86c84ced113dc8744
Ivan Zhelev <nvjcompany@gmail.com>
2019-11-30 15:54:05 +0200

    Update SecurityConfig

--- weather-app-spring/src/main/java/uni/fmi/st/security/SecurityConfig.java
+++ weather-app-spring/src/main/java/uni/fmi/st/security/SecurityConfig.java
@@ -3,18 +3,25 @@
  */
 package uni.fmi.st.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.security
 											.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 /**
  * Base spring security config class
@@ -29,6 +36,17 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 		jsr250Enabled = true
 		)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private UserDetailsService userDetailsService;
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	public SecurityConfig(ApplicationDetailsService userDetailsService
+							,PasswordEncoder passwordEncoder) {
+		this.userDetailsService =userDetailsService;
+		this.passwordEncoder =passwordEncoder;
+	}
+	
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -44,21 +62,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.and().csrf().disable();
 	}
 	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth)
+								throws Exception {
+		super.configure(auth);
+		auth.userDetailsService(userDetailsService)
+			.passwordEncoder(passwordEncoder);
+		
+	}
+	
 	
 	@Bean
-	@Override
-	protected UserDetailsService userDetailsService() {
-		UserDetails user = User.withDefaultPasswordEncoder()
-							.username("user")
-							.password("password")
-							.roles("USER")
-							.build();
-		UserDetails admin = User.withDefaultPasswordEncoder()
-							.username("admin")
-							.password("password")
-							.roles("USER", "ADMIN")
-							.build();
-		return new InMemoryUserDetailsManager(user, admin);
+	public SessionRegistry sessionRegistry() {
+		return new SessionRegistryImpl();
 	}
+
+	@Bean
+	public HttpSessionEventPublisher httpSessionEventPublisher() {
+		return new HttpSessionEventPublisher();
+	}
+
+	/**
+	 * The password encoder to use when encrypting passwords.
+	 */
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 
 }
